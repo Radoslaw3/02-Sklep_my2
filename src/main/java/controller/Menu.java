d67713f56dd428ab5cbf84e314a7f5f082f348a8
Radek <cossss@cosemail.com>
2018-06-16 21:54:17 +0200

    First commit

---
+++ src/main/java/controller/Menu.java
@@ -0,0 +1,12 @@
+package controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Menu {
+
+    public static void main(String[] args) {
+        ObjectMapper aaa = new ObjectMapper();
+
+
+    }
+}
